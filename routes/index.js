3fbc8db81ffbf2c30723f21b57b81fff8a12fd52
ViniciusVenancio <venanciovinicius1@hotmail.com>
2025-02-11 14:07:22 -0300

    criado /pessoa para cadastrar pessoa

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,22 @@ var express = require('express');
 var router = express.Router();
 const db = require("../db");
 
+//create de pessoa
+router.post('/pessoa', (req, res) => {
+  const {nome, email, nascimento} = req.body;
+
+  const query = 'INSERT INTO pessoa(nome, email, nascimento) VALUES (?, ?, ?)';
+  db.query(query, [nome, email, nascimento], (err, results) => {
+    if (err) {
+      console.log('Erro ao inserir: ' + err);
+      res.status(500).send('Erro ao inserir.');
+      return;
+    }
+    res.send('Usuário inserido com sucesso!');
+  });
+});
+
+//listar todas as pessoas
 router.get('/pessoas', (req, res) => {
   const query = 'SELECT * FROM pessoa';
   db.query(query, (err, results) => {
