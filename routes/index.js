624ece3973554ded43a4c52ab46530e89b2769e8
ViniciusVenancio <venanciovinicius1@hotmail.com>
2025-02-11 13:34:38 -0300

    criado /pessoas leitura de todos no Banco de Dados

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 const db = require("../db");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/pessoas', (req, res) => {
+  const query = 'SELECT * FROM pessoa';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log('Erro ao executar consulta: ' + err);
+      res.status(500).send('Erro ao executar consulta.');
+      return;
+    }
+    res.json(results);
+  });
 });
 
 module.exports = router;
